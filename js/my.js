330e4327b892ecb86c4fbf5682a8fbbb553ce700
thez3ro <silvethebest@yahoo.it>
2014-01-30 20:50:04 +0100

    Life

--- js/my.js
+++ js/my.js
@@ -1,22 +1,23 @@
-var a=$("body").height(),b=$("#woot").height()+$("#woot").css("margin-top")+$("#woot").css("margin-bottom");
+var a=$("body").height(),b=$("#footer").height()+$("#footer").css("margin-top").replace(/\px/g,"");+$("#footer").css("margin-bottom").replace(/\px/g,"");;
 alert(a);
 alert(b);
 
-
-if ( $("body").height() > $("#woot").height() ){
-  $("#woot").css("z-index",1);
-  $("#woot").css("position","fixed");
-  $("#woot").css("bottom","0px");
-  $("#woot").css("width","100%");
-  $("#woot").css("height",b);
-  $("#woot").css("overflow","hidden");
+/*
+if ( $("body").height() > $("#footer").height() ){
+  $("#footer").css("z-index",1);
+  $("#footer").css("position","fixed");
+  $("#footer").css("bottom","0px");
+  $("#footer").css("width","100%");
+  $("#footer").css("height",b);
+  $("#footer").css("overflow","hidden");
   $("#container").css("position","relative");
   $("#container").css("z-index",5);
   $("#container").css("margin-bottom",b);
   $("#container").css("box-shadow","0px 1px 7px rgba(0, 0, 0, 0.8)");
 } else {
-  $("#woot").css("z-index",1);
-  $("#woot").css("position","relative");
-  $("#woot").css("width","100%");
-  $("#woot").css("height",$("#woot").height());
+  $("#footer").css("z-index",1);
+  $("#footer").css("position","relative");
+  $("#footer").css("width","100%");
+  $("#footer").css("height",$("#footer").height());
 }
+*/
